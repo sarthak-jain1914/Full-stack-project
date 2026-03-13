22baa67fae5f6c9561225e861c689b8a94c2f8e6
sarthak_jain <sarthakjain9966@gmail.com>
2026-03-13 12:39:14 +0530

    implement axios for api calling

--- frontend/product-ui/src/App.js
+++ frontend/product-ui/src/App.js
@@ -4,33 +4,3 @@ function App() {
   return <ProductDashboard />;
 }
 export default App;
-    // <div>
-    //   <h2>Product Dashboard</h2>
-
-    //   {message && <p>{message}</p>}
-
-    //   <button onClick={loadProducts} style={{ marginRight: "10px" }}>
-    //     {products.length > 0 ? "Unload Products" : "Load Products"}
-    //   </button>
-
-    //   <button
-    //     onClick={() => setShowInputForm(!showInputForm)}
-    //     style={{ marginRight: "10px" }}
-    //   >
-    //     Create Product
-    //   </button>
-
-    //   <button
-    //     onClick={() => setisDeleted(!isDeleted)}
-    //     style={{ marginRight: "10px" }}
-    //   >
-    //     Delete Product
-    //   </button>
-
-    //   <button
-    //     onClick={() => setupdateFieldVisibility(!updateFieldVisibility)}
-    //     style={{ marginRight: "10px" }}
-    //   > update product
-    //   </button>
-    // </div>
-
--- frontend/product-ui/src/services/productService.js
+++ frontend/product-ui/src/services/productService.js
@@ -1,43 +1,30 @@
-// methods to interact with the API
+// methods to interact with the backend API
+import axios from "axios";
 
 const API_URL = "http://localhost:5000/api";
 
 export const getProducts = async () => {
-
-    const response = await fetch(`${API_URL}/products`);
-    return response.json();
-  };
-
+  const response = await axios.get(`${API_URL}/products`);
+  return response.data;
+};
 
 export const createProduct = async (description, price) => {
-    return await fetch(`${API_URL}/product`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        description,
-        price,
-      }),
-    });
-  };
-  export const updateProduct = async (id, description, price) => {
-    await fetch(`${API_URL}/product`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        updateId : id,
-        updateDescription: description,
-        updatePrice:price
-      }),
-    });
-  };
+  const response = await axios.post(`${API_URL}/product`, {
+    description,
+    price,
+  });
+  return response.data;
+};
 
-export const deleteProduct = async (id,description, price) => {
-    
-        await fetch(`${API_URL}/product/${id}`, {
-          method: "DELETE",
-        })
-      };
+export const updateProduct = async (id, description, price) => {
+  const response = await axios.put(`${API_URL}/product`, {
+    updateId: id,
+    updateDescription: description,
+    updatePrice: price,
+  });
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  await axios.delete(`${API_URL}/product/${id}`);
+};
